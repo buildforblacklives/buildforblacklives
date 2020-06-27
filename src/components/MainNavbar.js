76c0a56ed1d9c2a135bb3268b982d83b01e1eab1
Caroline Liu <caroline.liu111@gmail.com>
2020-06-26 23:12:01 -0400

    cleanup for PR

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -22,47 +22,6 @@ class MainNavbar extends React.Component {
           </LinkContainer>
         </Nav>
       </Navbar>
-      //   <nav className="navbar navbar-expand-lg navbar-styling">
-      //     <button
-      //       class="navbar-toggler"
-      //       type="button"
-      //       data-toggle="collapse"
-      //       data-target="#navbarNav"
-      //       aria-controls="navbarNav"
-      //       aria-expanded="false"
-      //       aria-label="Toggle navigation"
-      //     >
-      //       <span class="navbar-toggler-icon"></span>
-      //     </button>
-
-      //     <div class="collapse navbar-collapse" id="navbarNav">
-      //       <ul class="navbar-nav">
-      //         <li class="nav-item active">
-      //           <Link class="nav-link header-text" to="/">
-      //             Home
-      //           </Link>
-      //         </li>
-
-      //         <li class="nav-item">
-      //           <Link class="nav-link header-text" to="/about">
-      //             About
-      //           </Link>
-      //         </li>
-
-      //         <li class="nav-item">
-      //           <Link class="nav-link header-text" to="/contact">
-      //             Contact
-      //           </Link>
-      //         </li>
-
-      //         <li class="nav-item">
-      //           <a class="nav-link header-text" href="https://blm.sandboxnu.com">
-      //             Donate
-      //           </a>
-      //         </li>
-      //       </ul>
-      //     </div>
-      //   </nav>
     );
   }
 }
