1ddce1cdad343cbb11d3eb86f8c0fce4d8dc095f
Ifteda Ahmed-Syed <33703032+ifteda@users.noreply.github.com>
2020-06-28 13:15:35 -0400

    added buttons. resolved some PR comments

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../styling/about.css';
-
+import { Button, Col } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 {/* TODO 
-- scrolling
 - request a project button
 - open projects button
 - style cleanup
@@ -10,12 +10,14 @@ import '../styling/about.css';
 */}
 
 const AboutPage = () => (
-  <div class="container mt-5">
-    <div class="row">
-      <div class="col"></div>
-      <div class="col-8">
+  <div className="container mt-5">
+
+    <div className="row">
+      <div className="col"></div>
+      {/* TODO place image here (logo) */}
+      <div className="col-8">
         <h1>Build for Black Lives</h1>
-        <p class="p">
+        <p>
           This is a blurb about who we are and why we started this project and
           about how it is more about connecting people. Our mission and values 
           about supporting Black lives. We want allies to educate themselves and
@@ -32,15 +34,15 @@ const AboutPage = () => (
 
     <hr />
     
-    <div class="row">
-      <div class="col"></div>
-      <div class="col-10">
+    <div className="row">
+      <div className="col"></div>
+      <div className="col-10">
         <h2>Community Partners</h2>
-        <p class="p">
+        <p>
           These are organizations that have helped build, endorse, and promote 
           the platoform.
         </p>
-        <ul class="ul">
+        <ul>
           <li>HackBeanpot</li>
           <li>...</li>
           <li>...</li>
@@ -52,38 +54,54 @@ const AboutPage = () => (
           <li>...</li>
         </ul>
       </div>
+      <div className="col-8"></div>
     </div>
 
     <hr />
     
-    <div class="row">
-      <div class="col"></div>
-      <div class="col-8">
+    <div className="row">
+      <div className="col"></div>
+      <div className="col-8">
         <h2>Request a Project</h2>
-        <p class="p">
+        <p>
           Are you a black business owner, activist, protest organizer, or 
           community advocate looking for help building a website or online 
           platform, creating social media content, data visualizations, or other
            projects?
         </p>
         {/* TODO: add request button */}
+        <Col>
+          <LinkContainer to="/project-request">
+            <Button className="button" variant="warning" size="med">
+              Request a Project
+            </Button>
+          </LinkContainer>
+        </Col>
       </div>
     </div>
 
     <hr />
 
-    <div class="row">
-      <div class="col"></div>
-      <div class="col-10">
+    <div className="row">
+      <div className="col"></div>
+      <div className="col-10">
         <h2>Work on a Project</h2>
-        <p class="p">
+        <p>
           Are you a designer, programmer, engineer, data scientist, writer, web 
           developer, or marketing specialist looking to use your skills to do 
           pro bono work to support Black lives?
         </p>
         {/* TODO: add open projects button */}
+        <Col>
+        <LinkContainer to="/projects">
+          <Button className="button" variant="warning" size="med">
+            Work on a Project
+          </Button>
+        </LinkContainer>
+      </Col>
       </div>
     </div>
+    <div><br /></div>
   </div>
 );
 
