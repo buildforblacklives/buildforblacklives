ce1943e743a94d46f2edcaa45026eb6803920185
daniaalnahdi <alnahdi.d@husky.neu.edu>
2020-07-04 12:29:07 +0300

    clean up imports

--- src/containers/SiteContainer.js
+++ src/containers/SiteContainer.js
@@ -1,57 +1,58 @@
-import React from "react";
-import "../styling/site-container-styling.css";
-import LandingPage from "../components/LandingPage"
-import HomePage from "../pages/HomePage"
-import AboutPage from "../pages/AboutPage"
-import ContactUsPage from "../components/ContactUsPage"
-import {BrowserRouter as Router, Link, Route} from "react-router-dom";
-import NavBar from "../components/NavBar";
-
+import React from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import LandingPage from '../components/LandingPage';
+import HomePage from '../pages/HomePage';
+import AboutPage from '../pages/AboutPage';
+import ContactUsPage from '../components/ContactUsPage';
+import NavBar from '../components/NavBar';
+import '../styling/site-container-styling.css';
 
 class SiteContainer extends React.Component {
-    state = { }
+  state = {};
 
+  // contains the routing information for the site
+  render() {
+    return (
+      <div>
+        <Router>
+          <Route path="/" exact={true} render={() => <LandingPage />} />
 
-    // contains the routing information for the site
-    render() {
-        return(
-            <div>
-            <Router>
+          <Route
+            path="/home"
+            exact={true}
+            render={() => (
+              <div>
+                <NavBar />
+                <HomePage />
+              </div>
+            )}
+          />
 
-            <Route path="/"
-                   exact={true}
-                   render={()=> <LandingPage/> }/>
+          <Route
+            path="/about"
+            exact={true}
+            render={() => (
+              <div>
+                <NavBar />
+                <AboutPage />
+              </div>
+            )}
+          />
 
-
-            <Route path="/home"
-                   exact={true}
-                   render={()=>
-                        <div>
-                            <NavBar/>
-                            <HomePage/>
-                        </div>}/>
-
-
-            <Route path="/about"
-                   exact={true}
-                   render={()=>
-                        <div>
-                            <NavBar/>
-                            <AboutPage/>
-                        </div>}/>
-
-
-            <Route path="/contact"
-                   exact={true}
-                   render={()=>
-                        <div>
-                            <NavBar/>
-                            <ContactUsPage/>
-                        </div>}/>
-            </Router>
-            </div>
-        )
-    }
+          <Route
+            path="/contact"
+            exact={true}
+            render={() => (
+              <div>
+                <NavBar />
+                <ContactUsPage />
+              </div>
+            )}
+          />
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default SiteContainer
+export default SiteContainer;
