0894a17b2a8e315eb94651d05b0501bbfb80888c
snwessel <wessel.s@husky.neu.edu>
2020-07-04 18:19:55 -0400

    Updates based on feedback

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggle, NavbarCollapse, NavLink, Image } from 'react-bootstrap';
 import logo from '../assets/logo.png';
 
 class MainNavbar extends React.Component {
   render() {
     return (
       <Navbar variant="dark"  expand="lg" sticky="top">
-        <Navbar.Brand href="/">
-          <img src={logo} href="/" width="40" height="40" className="d-inline-block align-top" alt="Build for Black Lives" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <NavbarBrand href="/">
+          <Image src={logo} href="/" width="40" height="40"  className="d-inline-block align-top" alt="Build for Black Lives" />
+        </NavbarBrand>
+        <NavbarToggle aria-controls="basic-navbar-nav" />
+        <NavbarCollapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link id="navbar-text" href="/about">About</Nav.Link>
-            <Nav.Link id="navbar-text" href="/faq">FAQ</Nav.Link>
-            <Nav.Link id="navbar-text" href="/projects">Open Projects</Nav.Link>
-            <Nav.Link id="navbar-text" href="/project-request">Project Request</Nav.Link>
+            <NavLink id="navbar-text" href="/about">About</NavLink>
+            <NavLink id="navbar-text" href="/faq">FAQ</NavLink>
+            <NavLink id="navbar-text" href="/projects">Open Projects</NavLink>
+            <NavLink id="navbar-text" href="/project-request">Project Request</NavLink>
           </Nav>
-        </Navbar.Collapse>
+        </NavbarCollapse>
       </Navbar>
     );
   }
