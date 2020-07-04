106cbbac435298bebb40f9a17543f125aef2f30f
Ifteda Ahmed-Syed <33703032+ifteda@users.noreply.github.com>
2020-07-04 13:29:20 -0400

    Ifteda About Page 2nd Draft (#13)
    * Ifteda About Page 2nd Draft

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Link } from 'react-router-dom';
-{/* TODO
-  - images
-  - Contact Us icons
-  - style cleanup
-*/}
+import logo from '../media/blm.png';
+import fbLogo from '../assets/social_logos/Facebook_Logo.png';
+import igLogo from '../assets/social_logos/IG_Logo.png';
+import linkedinLogo from '../assets/social_logos/Linkedin_Logo.png';
+import twitterLogo from '../assets/social_logos/Twitter_Logo.png';
 
 const AboutPage = () => (
   <Container className="container" fluid="md">
-
     <Row>
-      {/* TODO place image here (logo) */}
-      <Col className="col"></Col>
-      <Col className="col-8">
+      <Col>
+        <Image src={logo} />
+      </Col>
+      <Col className="col-9">
         <h1>Build for Black Lives</h1>
         <p>
           Build for Black Lives, a platform created by HackBeanpot, connects 
@@ -40,8 +39,7 @@ const AboutPage = () => (
     <hr />
     
     <Row>
-      <Col className="col"></Col>
-      <Col className="col-10">
+      <Col>
         <h2>Community Partners</h2>
         <p>
           These are organizations that have helped build, endorse, and promote 
@@ -59,14 +57,23 @@ const AboutPage = () => (
           <li>...</li>
           <li>...</li> */}
         </ul>
+        <p>
+          Contact us if you would like to get involved. We are always looking to
+           partner with organizations who are commuitted to being anti-racist 
+           and want to help work toward justice and equity through tech. You can
+            also join our&nbsp;
+          <a href="https://join.slack.com/t/northeastern-hac3935/shared_invite/zt-fctqwcqe-7~n_GXliEYbQzUeqERHH6A">
+            Slack Workspace
+          </a>
+          &nbsp;to learn more from the organizing team.
+        </p>
       </Col>
     </Row>
 
     <hr />
     
     <Row>
-      <Col className="col"></Col>
-      <Col className="col-8">
+      <Col>
         <h2>Request a Project</h2>
         <p>
           Are you a black business owner, activist, protest organizer, or 
@@ -74,59 +81,72 @@ const AboutPage = () => (
           platform, creating social media content, data visualizations, or other
            projects?
         </p>
-        <Col>
-          <LinkContainer to="/project-request">
-            <Button className="button" variant="warning" size="med">
-              Request a Project
-            </Button>
-          </LinkContainer>
-        </Col>
+        <LinkContainer to="/project-request">
+          <Button className="button" variant="warning" size="med">
+            Request a Project
+          </Button>
+        </LinkContainer>
       </Col>
     </Row>
 
     <hr />
 
     <Row>
-      <Col className="col"></Col>
-      <Col className="col-10">
+      <Col>
         <h2>Work on a Project</h2>
         <p>
           Are you a designer, programmer, engineer, data scientist, writer, web 
           developer, or marketing specialist looking to use your skills to do 
           pro bono work to support Black lives?
         </p>
-        <Col>
-          <LinkContainer to="/projects">
-            <Button className="button" variant="warning" size="med">
-              Work on a Project
-            </Button>
-          </LinkContainer>
-        </Col>
+        <LinkContainer to="/projects">
+          <Button className="button" variant="warning" size="med">
+            Work on a Project
+          </Button>
+        </LinkContainer>
       </Col>
     </Row>
 
     <hr />
 
     <Row>
-      <Col className="col"></Col>
-      <Col className="col-8">
+      <Col>
         <h2>Contact Us</h2>
-        {/* TODO replace with icons */}
+        <p>
+          Feel free to reach out to us if you are interested in getting involved
+           or have questions! You can also check out our HackBeanpot social 
+           media pages for more information about what we do as an organization.
+        </p>
+        Email us at:&nbsp;
         <a href="mailto:buildforblacklives@hackbeanpot.com">
           buildforblacklives@hackbeanpot.com
-        </a> <br />
-        <a href="https://www.facebook.com/hackbeanpot/">
-          Facebook
-        </a> <br />
-        <a href="https://www.instagram.com/hackbeanpot/">
-          Instagram
-        </a> <br />
-        <a href="https://www.linkedin.com/company/hackbeanpot-inc">
-          LinkedIn
-        </a> <br />
-        <a href="https://twitter.com/hackbeanpot">
-          Twitter
         </a>
+        <br />
+        Visit us at:&nbsp;
+        <a href="https://hackbeanpot.com/"
+        target="_blank" rel="noopener noreferrer">
+          hackbeanpot.com
+        </a>
+        <Row>
+          <Col className="col-9">
+          <a href="https://www.facebook.com/hackbeanpot/" 
+          target="_blank" rel="noopener noreferrer">
+            <Image src={fbLogo} className="socialLogo" />
+          </a>
+          <a href="https://www.instagram.com/hackbeanpot/" 
+          target="_blank" rel="noopener noreferrer">
+            <Image src={igLogo} className="socialLogo" />
+          </a>
+          <a href="https://twitter.com/hackbeanpot" 
+          target="_blank" rel="noopener noreferrer">
+          <Image src={twitterLogo} className="socialLogo" />
+          </a>
+          <a href="https://www.linkedin.com/company/hackbeanpot-inc" 
+          target="_blank" rel="noopener noreferrer">
+            <Image src={linkedinLogo} className="socialLogo" />
+          </a>
+          </Col>
+        </Row>
       </Col>
     </Row>
 
