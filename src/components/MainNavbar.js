3bb5475eabf2129c11943f08d70e36b62c9681b6
snwessel <wessel.s@husky.neu.edu>
2020-07-04 18:35:27 -0400

    Debug changes

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Nav, Navbar, NavbarBrand, NavbarToggle, NavbarCollapse, NavLink, Image } from 'react-bootstrap';
+import { Nav, Navbar, NavbarBrand, NavLink, Image } from 'react-bootstrap';
 import logo from '../assets/logo.png';
 
 class MainNavbar extends React.Component {
@@ -10,15 +10,15 @@ class MainNavbar extends React.Component {
         <NavbarBrand href="/">
           <Image src={logo} href="/" width="40" height="40"  className="d-inline-block align-top" alt="Build for Black Lives" />
         </NavbarBrand>
-        <NavbarToggle aria-controls="basic-navbar-nav" />
-        <NavbarCollapse id="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <NavLink id="navbar-text" href="/about">About</NavLink>
             <NavLink id="navbar-text" href="/faq">FAQ</NavLink>
             <NavLink id="navbar-text" href="/projects-temp">Open Projects</NavLink>
             <NavLink id="navbar-text" href="/project-request">Project Request</NavLink>
           </Nav>
-        </NavbarCollapse>
+        </Navbar.Collapse>
       </Navbar>
     );
   }
