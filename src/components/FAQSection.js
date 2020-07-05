1988e0d9c6c0beb6b0528c81e7d9a81dbf6a29df
daniaalnahdi <33553341+daniaalnahdi@users.noreply.github.com>
2020-07-05 22:28:51 +0300

    Dania faq ui  (#21)
    * update faq ui
    
    * add mobile styling
    
    * clean up
    
    * remove unused function
    
    * add hover color on question
    
    * styling updates

--- src/components/FAQSection.js
+++ src/components/FAQSection.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import { Accordion, Card } from 'react-bootstrap';
+import React, { useContext } from 'react';
+import { Accordion, Card, Row, Col } from 'react-bootstrap';
+import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
+import AccordionContext from 'react-bootstrap/AccordionContext';
+
+import Plus from '../assets/plus';
+import Minus from '../assets/minus';
 import '../styling/FAQPage.css';
 
 class FAQSection extends React.Component {
   render() {
+    function IconToggleAccordion({ children, eventKey, callback }) {
+      const currentEventKey = useContext(AccordionContext);
+      const toggleIconOnClick = useAccordionToggle(eventKey, () => callback && callback(eventKey));
+      const isCurrentEventKey = currentEventKey === eventKey;
+
+      return (
+        <Card className="faq-accordion-card faq-accordion-question" onClick={toggleIconOnClick}>
+          <Row className="m-3">
+            <Col xs="auto"> {isCurrentEventKey ? <Minus /> : <Plus />} </Col>
+            <Col>{children}</Col>
+          </Row>
+        </Card>
+      );
+    }
     return (
       <section className="mt-1 mb-1">
-        <h3>Questions: {this.props.title}</h3>
+        <h3 className="mt-4 mb-4">Questions: {this.props.title}</h3>
         {this.props.faq.map((qa, i) => {
           return (
             <Accordion key={i} className="mt-3 mb-3">
+              <IconToggleAccordion as={Card.Header} eventKey="1">
+                {qa.question}
+              </IconToggleAccordion>
               <Card>
-                <Accordion.Toggle as={Card.Header} eventKey="1" className="faq-accordion">
-                  {`\u27A4 ${qa.question}`}
-                </Accordion.Toggle>
                 <Accordion.Collapse eventKey="1">
-                  <Card.Body className="faq-accordion">{qa.answer}</Card.Body>
+                  <Card.Body className="faq-accordion-card faq-accordion-answer">{qa.answer}</Card.Body>
                 </Accordion.Collapse>
               </Card>
             </Accordion>
