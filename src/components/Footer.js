bed73f057492a95fd5a61811ae08d583dd273c4e
snwessel <wessel.s@husky.neu.edu>
2020-07-05 14:15:04 -0400

    Update footer content & layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,37 +5,27 @@ import { Nav, Navbar, Col, Container, Image, Row  } from 'react-bootstrap';
 class Footer extends React.Component {
     render() {
       return (
-        <Navbar variant="dark" expand="lg" className="mt-4 p-4">
-              <Col className="col-md-6 text-right">
-                <a href="https://www.facebook.com/hackbeanpot/" 
-                target="_blank" rel="noopener noreferrer">
-                  <i className="fa fa-facebook-official fa-2x socialLogo footer-text" aria-hidden="true"></i>
-                </a>
-                <a href="https://www.instagram.com/hackbeanpot/" 
-                target="_blank" rel="noopener noreferrer">
-                  <i className="fa fa-instagram fa-2x socialLogo footer-text" aria-hidden="true"></i>
-                </a>
-                <a href="https://twitter.com/hackbeanpot" 
-                target="_blank" rel="noopener noreferrer">
-                  <i className="fa fa-twitter-square fa-2x socialLogo footer-text" aria-hidden="true"></i>
-                </a>
-                <a href="https://www.linkedin.com/company/hackbeanpot-inc" 
-                target="_blank" rel="noopener noreferrer">
-                  <i className="fa fa-linkedin-square fa-2x socialLogo footer-text" aria-hidden="true"></i>
-                </a>
-              </Col>
-              <Col className="col-md-6 mt-3">
-                <span className="footer-text">Email us at: </span>
-                <a href="mailto:buildforblacklives@hackbeanpot.com">
-                  buildforblacklives@hackbeanpot.com
-                </a>
-                <br />
-                <span className="footer-text">Visit us at: </span>
-                <a href="https://hackbeanpot.com/"
-                target="_blank" rel="noopener noreferrer">
-                  hackbeanpot.com
-                </a>
-              </Col>
+        <Navbar variant="dark" expand="lg" className="mt-4 p-4 justify-content-center">
+            <Col className="col-auto text-center text-nowrap">
+              <a href="https://www.facebook.com/Build-for-Black-Lives-105258644589291" 
+              target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-facebook fa-2x socialLogo footer-text" aria-hidden="true"></i>
+              </a>
+              <a href="mailto:buildforblacklives@hackbeanpot.com" 
+              target="_blank" rel="noopener noreferrer">
+                <i class="fas fa-envelope fa-2x socialLogo footer-text"></i>
+              </a>
+              <a href="https://join.slack.com/t/northeastern-hac3935/shared_invite/zt-fctqwcqe-7~n_GXliEYbQzUeqERHH6A" 
+              target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-slack fa-2x socialLogo footer-text" aria-hidden="true"></i>
+              </a>
+            </Col>
+            <Col className="col-auto mt-3 text-center">
+              <span className="footer-text">
+                Made in solidarity with 
+                <a href="https://blacklivesmatter.com/" target="_blank" rel="noopener noreferrer"> Black Lives Matter</a>
+              </span>
+            </Col>
         </Navbar>
       );
     }
