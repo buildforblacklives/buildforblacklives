4f77ba586633db91a991617f955953a6f919e911
Nupur Neogi <neogi.n@husky.neu.edu>
2020-07-07 23:18:36 -0400

    mailchimp signup form for subscription and styling

--- src/pages/ProjectsPageTemp.js
+++ src/pages/ProjectsPageTemp.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../styling/ProjectsPageTemp.css';
+import logo from '../assets/email-icon.png';
 
 const ProjectsPageTemp = () => (
   <div class="container mt-5">
@@ -6,22 +8,22 @@ const ProjectsPageTemp = () => (
       <div class="col"></div>
       <div class="col-8">
         <h1 class="mt-5">Projects Coming Soon</h1>
-        <h4 class="text-white mb-5">Stay tuned for our first projects!</h4>
+        <h4 class="mb-5">Stay tuned for upcoming projects!</h4>
+                 <p> We are in the process of reviewing projects and will be posting projects shortly.
+                        <br/> Check back in a couple weeks to see what projects have been added.</p>
       </div>
       <div class="col"></div>
     </div>
 
-    <hr class="solid" />
-
     <div class="row d-flex justify-content-center text-center">
       <div class="col-4">
-        <h6 class="mt-5 text-white"> Want to be notified? Enter your email below: </h6>
-        <input type="email" class="form-control" aria-describedby="email" placeholder="foo.bar@gmail.com" />
-        <button type="submit" class="btn btn-primary mt-3">
-          Submit
-        </button>
-      </div>
+        <img src={logo} width="40" class="mt-5 mb-3"/>
+        <h5> Want to be notified?</h5>
+
+        <a href="http://eepurl.com/g9JPtn" target="_blank" class="btn button btn-lg" aria-disabled="false"> Subscribe Here </a>
+
     </div>
+  </div>
   </div>
 );
 
