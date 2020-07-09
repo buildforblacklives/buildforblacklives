bcd1d938416ca88f8c6248d5002ae578a572b45a
Nupur Neogi <neogi.n@husky.neu.edu>
2020-07-08 23:06:53 -0400

    mobile updates

--- src/pages/ProjectsPageTemp.js
+++ src/pages/ProjectsPageTemp.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import '../styling/ProjectsPageTemp.css';
-import logo from '../assets/email-icon.png';
 
 const ProjectsPageTemp = () => (
   <div class="container mt-5">
-    <div class="row text-center">
-      <div class="col"></div>
-      <div class="col-8">
-        <h1 class="mt-5">Projects Coming Soon</h1>
-        <h4 class="mb-5">Stay tuned for upcoming projects!</h4>
-                 <p> We are in the process of reviewing projects and will be posting projects shortly.
-                        <br/> Check back in a couple weeks to see what projects have been added.</p>
-      </div>
-      <div class="col"></div>
-    </div>
-
     <div class="row d-flex justify-content-center text-center">
-      <div class="col-4">
-        <img src={logo} width="40" class="mt-5 mb-3"/>
-        <h5> Want to be notified?</h5>
+      <div class="col-md-8">
+        <h1 class="mt-5">Projects Coming Soon</h1>
+        <p> We are in the process of reviewing projects and will be posting projects shortly.
+            Check back in a couple weeks to see newly added projects.
+        </p>
+         <a href="http://eepurl.com/g9JPtn" target="_blank" class="btn btn-lg button mt-5" aria-disabled="false"> Notify Me About Projects </a>
 
-        <a href="http://eepurl.com/g9JPtn" target="_blank" class="btn button btn-lg" aria-disabled="false"> Subscribe Here </a>
-
-    </div>
+      </div>
   </div>
   </div>
 );
