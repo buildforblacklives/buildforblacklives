513ecaf2b435a03effa94fa2f4a4ac84158f8e9b
Felicia Zhang <zhang.fel@husky.neu.edu>
2020-07-12 15:33:26 -0400

    weird mobile stuff

--- src/assets/flow_diagrams/request_flow.js
+++ src/assets/flow_diagrams/request_flow.js
@@ -1,59 +1,59 @@
 import React from 'react'
 
 const RequestFlow = ({ className }) => (
-	<svg className={className} id="request-flow" xmlns="http://www.w3.org/2000/svg" width="849" height="225" viewBox="0 0 849 225">
-		<g id="Group_46" data-name="Group 46" transform="translate(301.875)">
-			<text id="_2" data-name="2" transform="translate(0.125 33)" fill="#f5cb4c" fontSize="34" fontFamily="HelveticaNeueMed, HelveticaNeue" fontWeight="500"><tspan x="0" y="0">2</tspan></text>
-			<g id="Group_43" data-name="Group 43" transform="translate(50.8 27.304)">
-				<rect id="Rectangle_42" data-name="Rectangle 42" width="108.078" height="106.94" rx="21" transform="translate(0 27.698)" fill="#f5cb4c" />
-				<rect id="Rectangle_22" data-name="Rectangle 22" width="108.078" height="106.94" rx="21" transform="translate(7.964 20.872)" fill="#f9f5e3" />
-				<path id="Path_12" data-name="Path 12" d="M804.492,517.6,833,557.956l66.854-80.391,11.134-13.389,2.93-3.523" transform="translate(-783.454 -453.828)" fill="none" stroke="#6bb4d6" strokeWidth="15" />
-				<path id="Path_14" data-name="Path 14" d="M804.492,517.6,833,557.956l49.274-59.251,31.644-38.051" transform="translate(-783.454 -460.654)" fill="none" stroke="#1f1f1f" strokeWidth="15" />
-			</g>
-			<text id="Receive_Approval" data-name="Receive Approval" transform="translate(2.125 219)" fill="#f9f5e3" fontSize="26" fontFamily="HelveticaNeueBold, HelveticaNeue" fontWeight="700"><tspan x="0" y="0">Receive Approval</tspan></text>
-		</g>
-		<g id="Group_45" data-name="Group 45" transform="translate(612.188)">
-			<text id="_3" data-name="3" transform="translate(-0.188 33)" fill="#f5cb4c" fontSize="34" fontFamily="HelveticaNeueMed, HelveticaNeue" fontWeight="500"><tspan x="0" y="0">3</tspan></text>
-			<g id="Group_42" data-name="Group 42" transform="translate(46.875 37.5)">
-				<rect id="Rectangle_48" data-name="Rectangle 48" width="138" height="106" rx="24" transform="translate(-0.062 7.5)" fill="#f5cb4c" />
-				<rect id="Rectangle_21" data-name="Rectangle 21" width="136" height="105" rx="24" transform="translate(7.938 0.5)" fill="#f9f5e3" />
-				<path id="Path_13" data-name="Path 13" d="M1058.764,491l68.318,46.555.042-.031L1189.245,491" transform="translate(-1047.725 -476.946)" fill="none" stroke="#6bb4d6" strokeWidth="8" />
-				<path id="Path_19" data-name="Path 19" d="M1058.764,491l71,49.865,64.6-49.865" transform="translate(-1050.853 -484.807)" fill="none" stroke="#1f1f1f" strokeWidth="8" />
-			</g>
-			<text id="Wait_for_Volunteers" data-name="Wait for Volunteers" transform="translate(1.813 219)" fill="#f9f5e3" fontSize="26" fontFamily="HelveticaNeueBold, HelveticaNeue" fontWeight="700"><tspan x="0" y="0">Wait for Volunteers</tspan></text>
-		</g>
-		<g id="Group_47" data-name="Group 47">
-			<text id="_1" data-name="1" transform="translate(0 33)" fill="#f5cb4c" fontSize="34" fontFamily="HelveticaNeueMed, HelveticaNeue" fontWeight="500"><tspan x="0" y="0">1</tspan></text>
-			<g id="Group_44" data-name="Group 44" transform="translate(56.25 23.437)">
-				<rect id="Rectangle_46" data-name="Rectangle 46" width="115" height="132" rx="20" transform="translate(-0.25 8.563)" fill="#f5cb4c" />
-				<rect id="Rectangle_20" data-name="Rectangle 20" width="113" height="133" rx="18" transform="translate(6.75 -0.438)" fill="#f9f5e3" />
-				<g id="Rectangle_43" data-name="Rectangle 43" transform="translate(18.75 21.563)" fill="#1f1f1f" stroke="#000" strokeWidth="1">
-					<rect width="90" height="16" stroke="none" />
-					<rect x="0.5" y="0.5" width="89" height="15" fill="none" />
-				</g>
-				<g id="Rectangle_44" data-name="Rectangle 44" transform="translate(18.75 50.563)" fill="#1f1f1f" stroke="#000" strokeWidth="1">
-					<rect width="90" height="8" stroke="none" />
-					<rect x="0.5" y="0.5" width="89" height="7" fill="none" />
-				</g>
-				<g id="Rectangle_45" data-name="Rectangle 45" transform="translate(18.75 71.563)" fill="#1f1f1f" stroke="#000" strokeWidth="1">
-					<rect width="90" height="24" stroke="none" />
-					<rect x="0.5" y="0.5" width="89" height="23" fill="none" />
-				</g>
-				<path id="Path_16" data-name="Path 16" d="M7.395,0H30.148a7.395,7.395,0,1,1,0,14.79H7.395A7.395,7.395,0,0,1,7.395,0Z" transform="translate(46.14 108.531)" fill="#6bb4d6" />
-			</g>
-			<text id="Submit_Request" data-name="Submit Request" transform="translate(13 219)" fill="#f9f5e3" fontSize="26" fontFamily="HelveticaNeueBold, HelveticaNeue" fontWeight="700"><tspan x="0" y="0">Submit Request</tspan></text>
-		</g>
-		<g id="Group_75" data-name="Group 75" transform="translate(218.001 80.924)">
-			<g id="Group_40" data-name="Group 40" transform="translate(0 0)">
-				<path id="Path_15" data-name="Path 15" d="M18.782,0,0,22.092,18.782,42.47" transform="matrix(-1, 0.017, -0.017, -1, 50.932, 41.427)" fill="none" stroke="#f9f5e3" strokeWidth="8" />
-			</g>
-		</g>
-		<g id="Group_75-2" data-name="Group 75" transform="translate(522.001 80.924)">
-			<g id="Group_40-2" data-name="Group 40" transform="translate(0 0)">
-				<path id="Path_15-2" data-name="Path 15" d="M18.782,0,0,22.092,18.782,42.47" transform="matrix(-1, 0.017, -0.017, -1, 50.932, 41.427)" fill="none" stroke="#f9f5e3" strokeWidth="8" />
-			</g>
-		</g>
-	</svg>
+  <svg className={className} id="request-flow" xmlns="http://www.w3.org/2000/svg" width="849" height="225" viewBox="0 0 849 225">
+    <g id="Group_46" data-name="Group 46" transform="translate(301.875)">
+      <text id="_2" data-name="2" transform="translate(0.125 33)" fill="#f5cb4c" fontSize="34" fontFamily="HelveticaNeueMed, HelveticaNeue" fontWeight="500"><tspan x="0" y="0">2</tspan></text>
+      <g id="Group_43" data-name="Group 43" transform="translate(50.8 27.304)">
+        <rect id="Rectangle_42" data-name="Rectangle 42" width="108.078" height="106.94" rx="21" transform="translate(0 27.698)" fill="#f5cb4c" />
+        <rect id="Rectangle_22" data-name="Rectangle 22" width="108.078" height="106.94" rx="21" transform="translate(7.964 20.872)" fill="#f9f5e3" />
+        <path id="Path_12" data-name="Path 12" d="M804.492,517.6,833,557.956l66.854-80.391,11.134-13.389,2.93-3.523" transform="translate(-783.454 -453.828)" fill="none" stroke="#6bb4d6" strokeWidth="15" />
+        <path id="Path_14" data-name="Path 14" d="M804.492,517.6,833,557.956l49.274-59.251,31.644-38.051" transform="translate(-783.454 -460.654)" fill="none" stroke="#1f1f1f" strokeWidth="15" />
+      </g>
+      <text id="Receive_Approval" data-name="Receive Approval" transform="translate(2.125 219)" fill="#f9f5e3" fontSize="26" fontFamily="HelveticaNeueBold, HelveticaNeue" fontWeight="700"><tspan x="0" y="0">Receive Approval</tspan></text>
+    </g>
+    <g id="Group_45" data-name="Group 45" transform="translate(612.188)">
+      <text id="_3" data-name="3" transform="translate(-0.188 33)" fill="#f5cb4c" fontSize="34" fontFamily="HelveticaNeueMed, HelveticaNeue" fontWeight="500"><tspan x="0" y="0">3</tspan></text>
+      <g id="Group_42" data-name="Group 42" transform="translate(46.875 37.5)">
+        <rect id="Rectangle_48" data-name="Rectangle 48" width="138" height="106" rx="24" transform="translate(-0.062 7.5)" fill="#f5cb4c" />
+        <rect id="Rectangle_21" data-name="Rectangle 21" width="136" height="105" rx="24" transform="translate(7.938 0.5)" fill="#f9f5e3" />
+        <path id="Path_13" data-name="Path 13" d="M1058.764,491l68.318,46.555.042-.031L1189.245,491" transform="translate(-1047.725 -476.946)" fill="none" stroke="#6bb4d6" strokeWidth="8" />
+        <path id="Path_19" data-name="Path 19" d="M1058.764,491l71,49.865,64.6-49.865" transform="translate(-1050.853 -484.807)" fill="none" stroke="#1f1f1f" strokeWidth="8" />
+      </g>
+      <text id="Wait_for_Volunteers" data-name="Wait for Volunteers" transform="translate(1.813 219)" fill="#f9f5e3" fontSize="26" fontFamily="HelveticaNeueBold, HelveticaNeue" fontWeight="700"><tspan x="0" y="0">Wait for Volunteers</tspan></text>
+    </g>
+    <g id="Group_47" data-name="Group 47">
+      <text id="_1" data-name="1" transform="translate(0 33)" fill="#f5cb4c" fontSize="34" fontFamily="HelveticaNeueMed, HelveticaNeue" fontWeight="500"><tspan x="0" y="0">1</tspan></text>
+      <g id="Group_44" data-name="Group 44" transform="translate(56.25 23.437)">
+        <rect id="Rectangle_46" data-name="Rectangle 46" width="115" height="132" rx="20" transform="translate(-0.25 8.563)" fill="#f5cb4c" />
+        <rect id="Rectangle_20" data-name="Rectangle 20" width="113" height="133" rx="18" transform="translate(6.75 -0.438)" fill="#f9f5e3" />
+        <g id="Rectangle_43" data-name="Rectangle 43" transform="translate(18.75 21.563)" fill="#1f1f1f" stroke="#000" strokeWidth="1">
+          <rect width="90" height="16" stroke="none" />
+          <rect x="0.5" y="0.5" width="89" height="15" fill="none" />
+        </g>
+        <g id="Rectangle_44" data-name="Rectangle 44" transform="translate(18.75 50.563)" fill="#1f1f1f" stroke="#000" strokeWidth="1">
+          <rect width="90" height="8" stroke="none" />
+          <rect x="0.5" y="0.5" width="89" height="7" fill="none" />
+        </g>
+        <g id="Rectangle_45" data-name="Rectangle 45" transform="translate(18.75 71.563)" fill="#1f1f1f" stroke="#000" strokeWidth="1">
+          <rect width="90" height="24" stroke="none" />
+          <rect x="0.5" y="0.5" width="89" height="23" fill="none" />
+        </g>
+        <path id="Path_16" data-name="Path 16" d="M7.395,0H30.148a7.395,7.395,0,1,1,0,14.79H7.395A7.395,7.395,0,0,1,7.395,0Z" transform="translate(46.14 108.531)" fill="#6bb4d6" />
+      </g>
+      <text id="Submit_Request" data-name="Submit Request" transform="translate(13 219)" fill="#f9f5e3" fontSize="26" fontFamily="HelveticaNeueBold, HelveticaNeue" fontWeight="700"><tspan x="0" y="0">Submit Request</tspan></text>
+    </g>
+    <g id="Group_75" data-name="Group 75" transform="translate(218.001 80.924)">
+      <g id="Group_40" data-name="Group 40" transform="translate(0 0)">
+        <path id="Path_15" data-name="Path 15" d="M18.782,0,0,22.092,18.782,42.47" transform="matrix(-1, 0.017, -0.017, -1, 50.932, 41.427)" fill="none" stroke="#f9f5e3" strokeWidth="8" />
+      </g>
+    </g>
+    <g id="Group_75-2" data-name="Group 75" transform="translate(522.001 80.924)">
+      <g id="Group_40-2" data-name="Group 40" transform="translate(0 0)">
+        <path id="Path_15-2" data-name="Path 15" d="M18.782,0,0,22.092,18.782,42.47" transform="matrix(-1, 0.017, -0.017, -1, 50.932, 41.427)" fill="none" stroke="#f9f5e3" strokeWidth="8" />
+      </g>
+    </g>
+  </svg>
 )
 
 export default RequestFlow
