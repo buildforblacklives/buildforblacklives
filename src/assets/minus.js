ca718749773b5920f13b5cbdb56a0d246bcc5461
daniaalnahdi <33553341+daniaalnahdi@users.noreply.github.com>
2020-07-12 21:07:21 +0300

    Dania faq updates (#38)
    * faq page style and copy updates
    
    * faq responsive styling
    
    * faq copy updates

--- src/assets/minus.js
+++ src/assets/minus.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Minus = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 45.724 10">
+const Minus = (props) => {
+  let dimensions = (props.small ? 24 : 32)
+ return <svg xmlns="http://www.w3.org/2000/svg" width={dimensions} height={dimensions} viewBox="0 0 45.724 10">
     <path id="Path_27" data-name="Path 27" d="M0,0H45.724" transform="translate(0 5)" fill="none" stroke="#6098BF" strokeWidth="10"/>
   </svg>
-)
+}
 
 export default Minus;
