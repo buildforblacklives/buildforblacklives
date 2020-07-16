e4916ae4ccffc3d7798db264fa92e64ee40f0385
Felicia Zhang <zhang.fel@husky.neu.edu>
2020-07-15 23:58:25 -0400

    projects page

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,126 +1,146 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Row, Col, Card, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import ProjectSelectFlow from '../assets/flow_diagrams/project_selection_flow';
 
 import '../styling/ProjectsPage.css';
 
-const { Body, Title, Subtitle, Text, Link } = Card;
+const { Body, Subtitle } = Card;
 
-const projects = [
-  { id: 0, title: 'Project Title 1', company: 'Organization Name 1' },
-  { id: 1, title: 'Project Title 2', company: 'Organization Name 2' },
-  { id: 2, title: 'Project Title 3', company: 'Organization Name 3' },
-  { id: 3, title: 'Project Title 4', company: 'Organization Name 4' },
-  { id: 4, title: 'Project Title 5', company: 'Organization Name 5' },
-  { id: 5, title: 'Project Title 6', company: 'Organization Name 6' },
-  { id: 6, title: 'Project Title 7', company: 'Organization Name 7' }
-];
+// example projects, delete this later
+const example = (num) => ({
+  id: num,
+  isUrgent: num %2 === 0,
+  orgName: 'HackBeanpot ' + num,
+  orgAbout:
+    'this is what our org does. Which likely has a lot of words, resulting in multiple lines on the screen. We love beans in our org. bean bean bean bean bean bean bean bean bean bean bean bean bean bean bean bean bean bean',
+  orgEmail: 'team@hbp.com',
+  orgPhone: null,
+  projectTitle: 'Sample Website Project ' + num,
+  projectAbout:
+    'This is what the project is about. Which is about beans. bean bean bean bean bean bean bean bean bean bean bean bean bean bean bean bean bean bean',
+  projectTime: 'Estimated 2 weeks'
+})
 
-class ProjectsPage extends React.Component {
-  state = {
-    selected: -1
-  };
+const sampleProjects = [example(1), example(2), example(3), example(4), example(5), example(6), example(7)];
 
-  render() {
-    const { selected } = this.state
+const ProjectCard = ({ project, isSelectedView, setSelected, selected }) => {
+  const { id, projectTitle, orgName, isUrgent, projectAbout } = project;
+  const getClass = () => {
+    let className = 'preview-card';
+    if (isSelectedView) {
+      className += ' select-view-preview-card'
 
-    return (
-      <>
-        {selected === -1 && (
-          <Container className="pg-container">
-            <Row className="d-flex justify-content-center">
-              <h1>Open Projects</h1>
-              <Row className="d-flex justify-content-left">
-                {projects.map((project) => (
-                  <Col lg={4}>
-                    <Card className="broad card-btn" onClick={() => this.setState({ selected: project.id })}>
-                      <Body>
-                        <h5> {project.title} </h5>
-                        <Subtitle className="mb-2 text-muted"> {project.company} </Subtitle>
-                        <Button className="urgent-tag"> Urgent </Button>
-                        <p class="card-text">
-                          Project description. We are looking for a group of people to help us put together a simple
-                          website for people to email their state reps.
-                        </p>
-                        <Link href="#" className="float-right" onClick={() => this.setState({ selected: project.id })}>
-                          More details...
-                        </Link>
-                      </Body>
-                    </Card>
-                  </Col>
-                ))}
-              </Row>
-            </Row>
-          </Container>
-        )}
-
-        {selected !== -1 && (
-          <Container className="pg-container center-div">
-            <Row>
-              <Col lg={4} className="overflow-auto scrolling project-col d-none d-lg-block">
-                {projects.map((project) => (
-                  <Card
-                    className={`preview-card ${selected !== project.id ? 'card-btn' : 'selected-btn'}`}
-                    onClick={() => this.setState({ selected: project.id })}
-                  >
-                    <Body>
-                      <Title> {project.title} </Title>
-                      <Subtitle className="mb-2 text-muted"> {project.company} </Subtitle>
-                      <Button className="urgent-tag"> Urgent </Button>
-                      <Text>
-                        Project description. We are looking for a group of people to help us put together a simple
-                        website for people to email their state reps.
-                      </Text>
-                      <Link href="#" className="float-right" onClick={() => this.setState({ selected: project.id })}>
-                        More details...
-                      </Link>
-                    </Body>
-                  </Card>
-                ))}
-              </Col>
-
-              <Col lg={7} md={12} className="">
-                <Card className="description-card">
-                  <Body>
-                    <div class="d-flex justify-content-lg-end justify-content-md-start">
-                      <Button className="close-btn mb-4 mb-lg-0" onClick={() => this.setState({ selected: -1 })}>
-                        Close
-                      </Button>
-                    </div>
-
-                    <h4> {projects[selected].title}</h4>
-                    <Subtitle className="mb-2 text-muted">{projects[selected].company}</Subtitle>
-                    <Button className="urgent-tag"> Urgent </Button>
-
-                    <h5 className="pt-3"> About {projects[selected].company} </h5>
-                    <p>
-                      This is a more detailed example about what a card could say. It would have information here about
-                      what the company does and how people can get involved.
-                    </p>
-
-                    <h5 className="pt-3"> About the Project </h5>
-                    <p>
-                      This is a more detailed example about what a card could say. It would have information here about
-                      what the company does and how people can get involved.
-                    </p>
-
-                    <h5 className="pt-3"> Project Timeline </h5>
-                    <p class="card-text">Estimated 2 weeks - Need ASAP </p>
-
-                    <div class="text-center">
-                      <LinkContainer to={`/projects/${selected}`}>
-                        <Button className="primary-button"> Work on this project! </Button>
-                      </LinkContainer>
-                    </div>
-                  </Body>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-        )}
-      </>
-    );
+      if (selected === id) {
+        className += ' is-select-preview-card'
+      }
+    }
+    return className
   }
+
+  return (
+    <Card
+      className={getClass()}
+      onClick={() => setSelected(id)}>
+      <Body>
+        <h5> {projectTitle} </h5>
+        <Subtitle className="mb-2 preview-subtitle">{orgName}</Subtitle>
+        {isUrgent ?
+          <span className="project-card-urgent">Urgent</span> :
+          <span className="project-tag-space" />}
+        <p className="project-card-description">{projectAbout}</p>
+        <div className="project-more-link float-right">
+          More details...
+        </div>
+      </Body>
+    </Card>
+  )
+}
+
+const SelectedCard = ({ project, setSelected }) => {
+  const { id, orgName, orgAbout, projectTitle, projectAbout, projectTime, isUrgent } = project;
+
+  return (
+    <Card className="project-description-card">
+      <Body>
+        <div className="project-description-close float-right" onClick={() => setSelected(-1)} />
+
+        <h4> {projectTitle}</h4>
+        <Subtitle className="mb-2 project-description-subtitle">{orgName}</Subtitle>
+        {isUrgent && <div className="project-card-urgent">Urgent </div>}
+
+        <h5 className="pt-3"> About {orgName} </h5>
+        <p>{orgAbout}</p>
+
+        <h5 className="pt-3"> About the Project </h5>
+        <p>{projectAbout}</p>
+
+        <h5 className="pt-3"> Project Timeline </h5>
+        <p className="card-text">{projectTime}</p>
+
+        <div className="text-center">
+          <LinkContainer to={`/projects/${id}`}>
+            <Button className="primary-button"> Work on this project! </Button>
+          </LinkContainer>
+        </div>
+      </Body>
+    </Card>
+  )
+}
+
+
+const ProjectsPage = () => {
+  const [selected, setSelected] = useState(-1);
+  const [projects, setProjects] = useState([]);
+  // TODO: get projects from airtable
+  useEffect(() => {
+    setProjects(sampleProjects)
+  })
+
+  const findSelected = () => (
+    projects.find((project) => project.id === selected)
+  )
+
+  return (
+    <Container className="projects-page">
+      {selected === -1 ? (
+        <>
+          <h1>Open Projects</h1>
+          <ProjectSelectFlow className="flow-images" />
+          <Row className="d-flex justify-content-left">
+            {projects.map((project) => (
+              <Col key={project.id} lg={4} md={6} sm={12}>                
+                <ProjectCard project={project} isSelectedView={false} setSelected={setSelected} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )
+      :
+      (
+        <>
+          <div className="projects-back" onClick={() => setSelected(-1)}>
+            {'< Back to Open Projects'}
+          </div>
+          <Row>
+            <Col lg={4} className="project-list-panel d-none d-lg-block">
+              {projects.map((project) =>
+                <ProjectCard
+                  project={project}
+                  isSelectedView={true}
+                  setSelected={setSelected}
+                  selected={selected}
+                />)}
+            </Col>
+
+            <Col lg={{ span: 7, offset: 1 }} md={12} className="">
+              <SelectedCard project={findSelected()} setSelected={setSelected} />
+            </Col>
+          </Row>
+        </>
+      )}
+    </Container>
+  );
 }
 
 export { ProjectsPage };
