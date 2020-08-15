73f0ebcdd60281a23e2c9b30874f5b75ce848386
Caroline Liu <caroline.liu111@gmail.com>
2020-08-15 19:37:29 -0400

    fixed css naming for projects page

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import Airtable from 'airtable'
+import Airtable from 'airtable';
 import { Button, Row, Col, Card, Container, Spinner } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import ProjectSelectFlow from '../assets/flow_diagrams/project_selection_flow';
 import { ProjectsPageTemp } from './ProjectsPageTemp';
-import { translateAirtableRecord } from '../state/utils'
+import { translateAirtableRecord } from '../state/utils';
 import { createProjects } from '../state/projects';
 
 import '../styling/ProjectsPage.css';
@@ -17,35 +17,29 @@ const ProjectCard = ({ project, isSelectedView, setSelected, selected }) => {
   const getClass = () => {
     let className = 'preview-card';
     if (isSelectedView) {
-      className += ' select-view-preview-card'
+      className += ' select-view-preview-card';
 
       if (selected === id) {
-        className += ' is-select-preview-card'
+        className += ' is-select-preview-card';
       }
     }
-    return className
-  }
+    return className;
+  };
 
   return (
-    <Card
-      className={getClass()}
-      onClick={() => setSelected(id)}>
+    <Card className={getClass()} onClick={() => setSelected(id)}>
       <Body>
         <div className="preview-card-contents">
           <h5> {projectTitle} </h5>
           <Subtitle className="mb-2 preview-subtitle">{orgName}</Subtitle>
-          {isUrgent ?
-            <span className="project-card-urgent">Urgent</span> :
-            <span className="project-tag-space" />}
+          {isUrgent ? <span className="project-card-urgent">Urgent</span> : <span className="project-tag-space" />}
           <p className="project-card-description">{projectAbout}</p>
         </div>
-        <div className="project-more-link">
-          More details...
-        </div>
+        <div className="project-more-link">More details...</div>
       </Body>
     </Card>
-  )
-}
+  );
+};
 
 const SelectedCard = ({ project, setSelected }) => {
   const { id, orgName, orgAbout, projectTitle, projectAbout, projectDeadline, isUrgent } = project;
@@ -63,8 +57,8 @@ const SelectedCard = ({ project, setSelected }) => {
           <>
             <h5 className="pt-3"> Project Timeline </h5>
             <p className="card-text">{projectDeadline}</p>
-          </>)
-        }
+          </>
+        )}
 
         <h5 className="pt-3"> About {orgName} </h5>
         <p>{orgAbout}</p>
@@ -79,50 +73,54 @@ const SelectedCard = ({ project, setSelected }) => {
         </div>
       </Body>
     </Card>
-  )
-}
-
+  );
+};
 
 const ProjectsPage = () => {
-  const dispatch = useDispatch()
-  const savedProjects = useSelector(state => state)
+  const dispatch = useDispatch();
+  const savedProjects = useSelector((state) => state);
   const [selected, setSelected] = useState(-1);
-  const [projects, setProjects] = useState(savedProjects)
-  const [hasLoaded, setHasLoaded] = useState(false)
+  const [projects, setProjects] = useState(savedProjects);
+  const [hasLoaded, setHasLoaded] = useState(false);
 
-  const findSelected = () => (
-    projects.find((project) => project.id === selected)
-  )
+  const findSelected = () => projects.find((project) => project.id === selected);
 
   useEffect(() => {
     const fetchProjects = async () => {
       const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_KEY }).base('appBzqG0sB4hqtE0I');
-      let airtableRecords = []
-      
-      base('Design projects').select({
-        view: "Approved Projects"
-      }).eachPage(async (records, fetchNextPage) => {
-        airtableRecords = records.map(record => translateAirtableRecord(record))
-        await dispatch(createProjects(airtableRecords))
-        setProjects(airtableRecords)
-        setHasLoaded(true)
-        
-        fetchNextPage();
-      }, (err) => {
-        if (err) { console.error(err); return; }
-      })
-    }
+      let airtableRecords = [];
+
+      base('Design projects')
+        .select({
+          view: 'Approved Projects'
+        })
+        .eachPage(
+          async (records, fetchNextPage) => {
+            airtableRecords = records.map((record) => translateAirtableRecord(record));
+            await dispatch(createProjects(airtableRecords));
+            setProjects(airtableRecords);
+            setHasLoaded(true);
+
+            fetchNextPage();
+          },
+          (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          }
+        );
+    };
 
     if (savedProjects.length === 0) {
-      fetchProjects()
+      fetchProjects();
     } else {
-      setHasLoaded(true)
+      setHasLoaded(true);
     }
-  }, [dispatch, savedProjects, hasLoaded])
-
+  }, [dispatch, savedProjects, hasLoaded]);
 
   if (hasLoaded && projects.length === 0) {
-    return <ProjectsPageTemp />
+    return <ProjectsPageTemp />;
   }
 
   return (
@@ -140,7 +138,7 @@ const ProjectsPage = () => {
               ))}
             </Row>
           ) : (
-            <Row className="justify-content-center spinner">
+            <Row className="justify-content-center project-loading-spinner">
               <Spinner animation="border" variant="warning" />
             </Row>
           )}
@@ -160,22 +158,16 @@ const ProjectsPage = () => {
             </Button>
           </Row>
         </>
-      )
-      :
-      (
+      ) : (
         <>
           <div className="projects-back" onClick={() => setSelected(-1)}>
             {'< Back to Open Projects'}
           </div>
           <Row>
             <Col lg={4} className="project-list-panel">
-              {projects.map((project) =>
-                <ProjectCard
-                  project={project}
-                  isSelectedView={true}
-                  setSelected={setSelected}
-                  selected={selected}
-                />)}
+              {projects.map((project) => (
+                <ProjectCard project={project} isSelectedView={true} setSelected={setSelected} selected={selected} />
+              ))}
             </Col>
 
             <Col lg={8} md={12} className="">
@@ -186,6 +178,6 @@ const ProjectsPage = () => {
       )}
     </Container>
   );
-}
+};
 
 export { ProjectsPage };
