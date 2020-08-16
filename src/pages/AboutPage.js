6e87de37f0a52a403f5bc1941e3c42d41d9c2a29
Nupur Neogi <neogi.n@husky.neu.edu>
2020-08-16 11:35:07 -0400

    move resources into its own file

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 import Logo from '../assets/logo.js';
-import CommPartnersIcon from '../assets/community-partners.js';
-import ReqProjectIcon from '../assets/form-icon.js';
-import WorkProjectIcon from '../assets/project-select-icon.js';
 import MailIcon from '../assets/email-icon.js';
 
 import TechDesign from '../assets/tech-design.js';
 
 import '../styling/AboutPage.css';
 
-const communityPartners = ['HackBeanpot', 'Unofficial Committee'];
+import { ResourceItems } from '../data/resources';
 
 const AboutPage = () => (
   <Container className="about-page" fluid>
@@ -92,120 +88,19 @@ const AboutPage = () => (
     <Row className="row-align-center">
       <Col xs={12} md={8}>
         <h2>Anti-Racist Resources</h2>
-        <Row className="row-align-center">
-          <Col>
-            <a
-              className="heading-link"
-              href="https://thecreativeindependent.com/guides/how-to-think-differently-about-doing-good-as-a-creative-person/"
-            >
-              Avoiding the "Creative Savior Complex"
-            </a>
-            <p>
-              How to think differently about doing good. This resource is "A guide to avoiding 'creative savior complex'
-              when working on social impact projects, written by Omayeli Arenyeka and illustrated by Neta Bomani".
-            </p>
-          </Col>
-        </Row>
-
-        <Row className="row-align-center">
-          <Col>
-            <a
-              className="heading-link"
-              href="https://thecreativeindependent.com/guides/how-to-begin-designing-for-diversity/"
-            >
-              'Designing for Diversity'
-            </a>
-            <p>
-              A guide to get you started in building equitable products, services, and content, written by Boyuan Gao
-              and Jahan Mantin of Project Inkblot, with illustrations by Fanny Luor.
-            </p>
-          </Col>
-        </Row>
-
-        <Row className="row-align-center">
-          <Col>
-            <a
-              className="heading-link"
-              href="https://thecreativeindependent.com/guides/how-to-begin-designing-for-diversity/"
-            >
-              Books by Black Scholars in Tech
-            </a>
-            <p>
-              15+ Books by Black Scholars that members of the tech industry should read. Compiled by UCLAs Center for
-              Critical Internet Inquiry.
-            </p>
-          </Col>
-        </Row>
-
-        <Row className="row-align-center">
-          <Col>
-            <a className="heading-link" href="https://www.instagram.com/p/CCLz13Kjhhq/?igshid=hcs3d4rycdi4">
-              Dismantling Racism Workbook
-            </a>
-            <p>
-              A great informational resources and workbook developed initially by Kenneth Jones and Tema Okun. Find more
-              resources at
-              <span>
-                <a href="https://www.dismantlingracism.org/"> dismantlingracism.org</a>
-              </span>
-            </p>
-          </Col>
-        </Row>
-
-        <Row className="row-align-center">
-          <Col>
-            <a
-              className="heading-link"
-              href="https://docs.google.com/document/d/1PrAq4iBNb4nVIcTsLcNlW8zjaQXBLkWayL8EaPlh0bc/preview?fbclid=IwAR2cN7t9bK0bl0wdlBIyz2JiP2yRZHU031y6kNfGuR8jfEIKRTTPnh0RSY0&pru=AAABcp6aoew*gIcrg1J8mnVGpEPF6vgIfw"
-            >
-              Being an Anti-Racist White Ally
-            </a>
-            <p>
-              A long list of resources with clear progressions and steps for unlearning white supremacy and working
-              towards becoming an anti-racist ally. Document created by Anna Stamborski, Nikki Zimmermann, and Bailie
-              Gregory.
-            </p>
-          </Col>
-        </Row>
-
-        <Row className="row-align-center">
-          <Col>
-            <a
-              className="heading-link"
-              href="https://www.mapping-access.com/blog-1/2020/6/2/anti-racist-critical-deisgn"
-            >
-              Design Commitments to Abolish White Supremacy
-            </a>
-            <p>
-              A statement put out by the Critical Design Lab on action steps and Design commitments to abolish white
-              supremacy
-            </p>
-          </Col>
-        </Row>
-
-        <Row className="row-align-center">
-          <Col>
-            <a className="heading-link" href="https://www.instagram.com/p/CCLz13Kjhhq/?igshid=hcs3d4rycdi4">
-              White Savior Complex (short summary)
-            </a>
-            <p>
-              Important to consider why you are participating in this initiative, and how you talk about the work you
-              are doing. Created by @eisellety on Instagram.
-            </p>
-          </Col>
-        </Row>
-
-        <Row className="row-align-center">
-          <Col>
-            <a className="heading-link" href="http://thepoliticsofdesign.com/">
-              'The Politics of Design'
-            </a>
-            <p>
-              Working towards making design more responsible, anti-racist, and inclusive. This blog has posts written by
-              a range of writers discussing "The Politics of Design"
-            </p>
-          </Col>
-        </Row>
+        { ResourceItems.map((resource, i) =>
+            <Row className="row-align-center" key={i}>
+              <Col>
+                <a
+                  className="heading-link"
+                  href={resource.link}
+                >
+                  {resource.title}
+                </a>
+                {resource.description}
+              </Col>
+            </Row>
+         )}
 
         <Row className="row-align-center mt-3">
           <Col>
