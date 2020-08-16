c4f1fccc70e04aa183126f139001b30bfe2875fe
Caroline Liu <caroline.liu111@gmail.com>
2020-08-16 00:33:18 -0400

    copy changes

--- src/pages/CommPartnersPage.js
+++ src/pages/CommPartnersPage.js
@@ -9,7 +9,6 @@ import '../styling/CommPartnersPage.css';
 
 const outreachPartners = [
   { name: 'HackBeanpot', url: 'https://hackbeanpot.com/' },
-  { name: 'Sandbox', url: 'https://www.sandboxnu.com/' },
   { name: 'Khoury College of Computer Sciences', url: 'https://www.khoury.northeastern.edu/' },
   { name: 'Unofficial Committee', url: 'https://www.unofficialcommittee.com/' },
   { name: 'Common Impact', url: 'https://commonimpact.org/' }
@@ -17,7 +16,8 @@ const outreachPartners = [
 
 const servicePartners = [
   { name: 'Unofficial Committee', url: 'https://www.unofficialcommittee.com/' },
-  { name: 'Support Community', url: 'https://supportcommunity.com/' }
+  { name: 'Support Community, Inc', url: 'https://supportcommunity.com/' },
+  { name: 'Community Design Studio, Lesley Art + Design', url: 'http://lesley.smartcatalogiq.com/en/2020-2021/Undergraduate-Academic-Catalog/Undergraduate-Programs-and-Courses/College-of-Art-and-Design-Courses/IDESN-Design/3000/IDESN-3370' },
 ];
 
 const CommPartnersPage = () => (
@@ -32,9 +32,8 @@ const CommPartnersPage = () => (
       <Col>
         <p>
           The success of Build for Black Lives as a platform would not be possible without our Community Partners, who
-          have helped us expand the reach of Build for Black Lives by sharing with their respective audiences, offering
-          their skills to help build out projects on our platform, or suggesting other ways they would like to help
-          support our cause.
+          have helped us expand the reach of Build for Black Lives by sharing with their respective audiences or offering
+          their skills to help build out projects on our platform.
         </p>
       </Col>
     </Row>
@@ -46,7 +45,7 @@ const CommPartnersPage = () => (
         <h2>Outreach</h2>
         <p>Do you have an audience that would be a good fit for our platform, as either clients or volunteers?</p>
         <p>
-          We would love your help in spreading the word comm the existence of our platform! Share on your social media,
+          We would love your help in spreading the word about our platform! Share on your social media,
           help promote our platform, and email local businesses. We'll provide you the media assets and email templates
           to get started.
         </p>
@@ -73,8 +72,8 @@ const CommPartnersPage = () => (
       <Col>
         <h2>Service</h2>
         <p>
-          Is your organization able to provide a resource that would be invaluable to building out the project requests
-          on our platform or help us grow our platform?
+          Is your organization able to provide a resource that would be invaluable to building out any of the project requests
+          on our platform?
         </p>
         <p>
           We've really appreciated the community reaching out to us in wanting to support our platform and would love
@@ -98,7 +97,7 @@ const CommPartnersPage = () => (
         <h2>Become a Community Partner</h2>
         <p>
           You can become a Community Partner by helping us with outreach or providing your services to help with
-          projects. If you feel like is there another way that you can help us with our cause, we'd love to hear it! We
+          projects. If you feel like there is another way that you can help us with our cause, we'd love to hear it! We
           are always open to partnering with organizations that are committed to fighting racial inequity.
         </p>
         <p>
