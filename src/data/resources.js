b5e0d11f746a5bb1dda03dca59def3d1e6e7d652
Nupur Neogi <33645465+nupur-n@users.noreply.github.com>
2020-08-16 22:23:18 -0400

    About page and resources combined (#64)
    * Site Fixes (#52)
    
    * Cari margin fix (#46)
    
    * flow image fix
    
    * center title
    
    * left-align title
    
    * Full site copy updates (#47)
    
    * image margin sizing adjustments (#48)
    
    * about imgs
    
    * about stuff
    
    * resource and temp page
    
    * smaller
    
    * left
    
    * last?
    
    * console
    
    * about imgs
    
    * about stuff
    
    * resource and temp page
    
    * smaller
    
    * left
    
    * last?
    
    * console
    
    * request header
    
    Co-authored-by: Caroline Liu <caroline.liu111@gmail.com>
    
    * Raquel copy (#50)
    
    * Full site copy updates
    
    * fixed (#51)
    
    Co-authored-by: Raquel Levy <35646692+Raquellevy@users.noreply.github.com>
    Co-authored-by: Felicia Zhang <zhang.fel@husky.neu.edu>
    
    * [21.07.2020] deploy (#58)
    
    * Cari margin fix (#46)
    
    * flow image fix
    
    * center title
    
    * left-align title
    
    * Full site copy updates (#47)
    
    * image margin sizing adjustments (#48)
    
    * about imgs
    
    * about stuff
    
    * resource and temp page
    
    * smaller
    
    * left
    
    * last?
    
    * console
    
    * about imgs
    
    * about stuff
    
    * resource and temp page
    
    * smaller
    
    * left
    
    * last?
    
    * console
    
    * request header
    
    Co-authored-by: Caroline Liu <caroline.liu111@gmail.com>
    
    * Raquel copy (#50)
    
    * Full site copy updates
    
    * fixed (#51)
    
    * fixed footer underline (#53)
    
    * Dania fixes (#54)
    
    * remove blue outline on focus
    
    * add info to volunteer faq
    
    * fix text cutting off on safari
    
    * uncomment updates
    
    * standardize svg sizing
    
    * projects page (#55)
    
    * connect to airtable (#57)
    
    * connect to airtable
    
    * card sizes
    
    * faded
    
    * img
    
    Co-authored-by: Caroline Liu <caroline.liu111@gmail.com>
    Co-authored-by: Raquel Levy <35646692+Raquellevy@users.noreply.github.com>
    Co-authored-by: daniaalnahdi <33553341+daniaalnahdi@users.noreply.github.com>
    
    * transfer resources onto about page
    
    * revised logo and some spacing for combining these pages
    
    * fixed css naming for projects page
    
    * renamed styling components on about page
    
    * removed unused css from about
    
    * fix up alignment of headers and delete resources page
    
    * move resources into its own file
    
    * changed alignment for resources
    
    * minor h2 styling fix
    
    * match cp and about page icon styling
    
    * smooth scroll and fix centering for resources
    
    Co-authored-by: Caroline Liu <caroline.liu111@gmail.com>
    Co-authored-by: Raquel Levy <35646692+Raquellevy@users.noreply.github.com>
    Co-authored-by: Felicia Zhang <zhang.fel@husky.neu.edu>
    Co-authored-by: daniaalnahdi <33553341+daniaalnahdi@users.noreply.github.com>
    Co-authored-by: Nupur Neogi <neogi.n@husky.neu.edu>

---
+++ src/data/resources.js
@@ -0,0 +1,90 @@
+import React from 'react';
+
+const ResourceItems = [
+    {
+      link: "https://thecreativeindependent.com/guides/how-to-think-differently-about-doing-good-as-a-creative-person/",
+      title: 'Avoiding the "Creative Savior Complex"',
+      description: (
+        <p>
+          How to think differently about doing good. This resource is "A guide to avoiding 'creative savior complex'
+          when working on social impact projects, written by Omayeli Arenyeka and illustrated by Neta Bomani".
+        </p>
+       )
+    },
+    {
+      link: "https://thecreativeindependent.com/guides/how-to-begin-designing-for-diversity/",
+      title: 'Designing for Diversity',
+      description: (
+        <p>
+          A guide to get you started in building equitable products, services, and content, written by Boyuan Gao
+          and Jahan Mantin of Project Inkblot, with illustrations by Fanny Luor.
+        </p>
+       )
+    },
+    {
+      link: "https://www.c2i2.ucla.edu/racial-justice-and-tech/",
+      title: 'Books by Black Scholars in Tech',
+      description: (
+        <p>
+          15+ Books by Black Scholars that members of the tech industry should read. Compiled by UCLAs Center for
+          Critical Internet Inquiry.
+        </p>
+       )
+    },
+    {
+      link: "https://www.instagram.com/p/CCLz13Kjhhq/?igshid=hcs3d4rycdi4",
+      title: 'Dismantling Racism Workbook',
+      description: (
+        <p>
+          A great informational resources and workbook developed initially by Kenneth Jones and Tema Okun. Find more
+          resources at
+          <span>
+            <a href="https://www.dismantlingracism.org/"> dismantlingracism.org</a>
+          </span>
+        </p>
+       )
+    },
+    {
+      link: "https://docs.google.com/document/d/1PrAq4iBNb4nVIcTsLcNlW8zjaQXBLkWayL8EaPlh0bc/preview?fbclid=IwAR2cN7t9bK0bl0wdlBIyz2JiP2yRZHU031y6kNfGuR8jfEIKRTTPnh0RSY0&pru=AAABcp6aoew*gIcrg1J8mnVGpEPF6vgIfw",
+      title: 'Being an Anti-Racist White Ally',
+      description: (
+        <p>
+          A long list of resources with clear progressions and steps for unlearning white supremacy and working
+          towards becoming an anti-racist ally. Document created by Anna Stamborski, Nikki Zimmermann, and Bailie
+          Gregory.
+        </p>
+       )
+    },
+    {
+      link: "https://www.mapping-access.com/blog-1/2020/6/2/anti-racist-critical-deisgn",
+      title: 'Design Commitments to Abolish White Supremacy',
+      description: (
+        <p>
+          A statement put out by the Critical Design Lab on action steps and Design commitments to abolish white
+          supremacy
+        </p>
+       )
+    },
+    {
+      link: "https://www.instagram.com/p/CCLz13Kjhhq/?igshid=hcs3d4rycdi4",
+      title: 'White Savior Complex (short summary)',
+      description: (
+        <p>
+          Important to consider why you are participating in this initiative, and how you talk about the work you
+          are doing. Created by @eisellety on Instagram.
+        </p>
+       )
+    },
+    {
+      link: "http://thepoliticsofdesign.com/",
+      title: 'The Politics of Design',
+      description: (
+        <p>
+          Working towards making design more responsible, anti-racist, and inclusive. This blog has posts written by
+          a range of writers discussing "The Politics of Design"
+        </p>
+       )
+    },
+];
+
+export { ResourceItems };
