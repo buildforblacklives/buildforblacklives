44067b4be8ac5929f53c399bbcfc15d19a02da8e
daniaalnahdi <alnahdi.d@husky.neu.edu>
2020-10-02 12:14:37 +0300

    refactor faq page

--- src/pages/FAQPage.js
+++ src/pages/FAQPage.js
@@ -1,37 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Tabs, Tab } from 'react-bootstrap';
 
 import CustomAccordion from '../components/CustomAccordion';
+import useIsMobile from '../hooks/useIsMobile';
 import { FAQItems } from '../data/faq';
 
 import '../index.css';
 import '../styling/FAQPage.css';
 
-class FAQPage extends React.Component {
-  constructor(props) {
-    super(props);
-    // set to clientFAQ initially
-    this.state = {
-      faqType: 'client',
-      faqData: FAQItems.clientFAQ,
-      isMobile: null
-    };
-  }
+const FAQPage = () => {
+  const [faqData, setFaqData] = useState(FAQItems.clientFAQ);
+  const isMobile = useIsMobile();
 
-  componentDidMount() {
-    this.updateScreenSize();
-    window.addEventListener('resize', this.updateScreenSize.bind(this));
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateScreenSize.bind(this));
-  }
-
-  updateScreenSize() {
-    this.setState({ isMobile: window.innerWidth <= 992 });
-  }
-
-  handleClick = (e) => {
+  const handleTabClick = (e) => {
     // Prevent state change on container click
     if (e.target.tagName !== 'NAV') {
       const {
@@ -54,30 +35,27 @@ class FAQPage extends React.Component {
         default:
           selectedData = FAQItems.clientFAQ;
       }
-
-      this.setState({ faqType: selectedType, faqData: selectedData });
+      setFaqData(selectedData);
     }
   };
 
-  render() {
-    return (
-      <Container id="faq-page" className="justify-content-md-center">
-        <h1 className="mb-3">FAQs</h1>
-        <Tabs xs="auto" onClick={this.handleClick}>
-          <Tab eventKey="client" title={this.state.isMobile ? 'Request' : 'Request a Project'}></Tab>
-          <Tab eventKey="volunteer" title={this.state.isMobile ? 'Work' : 'Work on a Project'}></Tab>
-          <Tab eventKey="initiative" title={this.state.isMobile ? 'About' : 'About This Initiative'}></Tab>
-        </Tabs>
-        {!!this.state.faqData && (
-          <section className="accordion-section-container">
-            {this.state.faqData.map((qa, i) => {
-              return <CustomAccordion key={i} header={qa.question} body={qa.answer} />;
-            })}
-          </section>
-        )}
-      </Container>
-    );
-  }
-}
+  return (
+    <Container id="faq-page" className="justify-content-md-center">
+      <h1 className="mb-3">FAQs</h1>
+      <Tabs xs="auto" onClick={handleTabClick}>
+        <Tab eventKey="client" title={isMobile ? 'Request' : 'Request a Project'}></Tab>
+        <Tab eventKey="volunteer" title={isMobile ? 'Work' : 'Work on a Project'}></Tab>
+        <Tab eventKey="initiative" title={isMobile ? 'About' : 'About This Initiative'}></Tab>
+      </Tabs>
+      {!!faqData && (
+        <section className="accordion-section-container">
+          {faqData.map((qa, i) => {
+            return <CustomAccordion key={i} header={qa.question} body={qa.answer} />;
+          })}
+        </section>
+      )}
+    </Container>
+  );
+};
 
 export { FAQPage };
