ad5273f5c7ea28a91adad471a11890fdc63fa847
Nupur Neogi <33645465+nupur-n@users.noreply.github.com>
2020-11-04 11:59:04 -0500

    Revert "Closed Project Page (#100)" (#101)
    This reverts commit 910504e22798e80accaddea843392abfbc4c4a4b.

--- src/state/projects.js
+++ src/state/projects.js
@@ -1,31 +1,20 @@
 // DEFAULT STATE
-const initialState = {
-  openProjects: [],
-  matchedProjects: []
-}
+const initialState = []
 
 // ACTIONS
 const UPDATE_PROJECTS = 'UPDATE_PROJECTS'
-const UPDATE_MATCHED_PROJECTS = "UPDATE_MATCHED_PROJECTS"
 
 export const createProjects = projects => ({
   type: UPDATE_PROJECTS, projects
 })
 
-export const createMatchedProjects = projects => ({
-  type: UPDATE_MATCHED_PROJECTS, projects
-})
-
 // REDUCERS
-const handleSetProjects = (state, action) => ({ ...state, openProjects: action.projects })
-const handleSetMatchedProjects = (state, action) => ({ ...state, matchedProjects: action.projects })
+const handleSetProjects = (state, action) => action.projects
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_PROJECTS:
       return handleSetProjects(state, action)
-    case UPDATE_MATCHED_PROJECTS:
-      return handleSetMatchedProjects(state, action)
     default:
       return state;
   }
